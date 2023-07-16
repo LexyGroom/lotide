9db102ac3cbd4e6a1adcc3e1ce22f1120f975d9b
Alexys Groom <lexygroom@gmail.com>
2023-07-15 20:44:13 -0400

    created tail function

---
+++ tail.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array hasn't been changed
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+/*
+1. create a function to return everything except the first item of the provided array
+2. don't modify the existing array instead create a new array
+3. an array with 1 element should yield empty for tail
+4. an empty array should yield empty for tail
+*/
