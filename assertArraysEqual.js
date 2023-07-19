ada9ea250af1d1e9b1a12cdd369e1f2dad334ed2
Alexys Groom <lexygroom@gmail.com>
2023-07-19 19:14:32 -0400

    Implemented the assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,36 @@
+/*Based on that result, it will then print a message to the console indicating whether the arrays are equal or not. 
+If as per eqArrays the two input arrays are equal it will print something like "These arrays are equal", 
+if not equal it could print "These arrays are not equal".
+*/
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅ Array Assertion Passed.")
+  } else {
+    console.log("🛑 Array Assertion ")
+  }
+}
+
+//TESTING
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should fail
