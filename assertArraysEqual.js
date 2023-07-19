8f6d400cf980cb835492565e7e46920b7699723e
Alexys Groom <lexygroom@gmail.com>
2023-07-19 19:56:55 -0400

    Removed assertEqual function from assertArraysEqual.js as it's no longer needed. Added a period to console.log on fail. Linted assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-/*Based on that result, it will then print a message to the console indicating whether the arrays are equal or not. 
-If as per eqArrays the two input arrays are equal it will print something like "These arrays are equal", 
-if not equal it could print "These arrays are not equal".
-*/
-
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -23,11 +10,11 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log("✅ Array Assertion Passed.")
+    console.log("✅ Array Assertion Passed.");
   } else {
-    console.log("🛑 Array Assertion Failed")
+    console.log("🛑 Array Assertion Failed.");
   }
-}
+};
 
 //TESTING
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should pass
