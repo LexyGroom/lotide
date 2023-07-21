7c495561fe4971c00e042bbaf1e86b8a1de8cbd1
Alexys Groom <lexygroom@gmail.com>
2023-07-21 10:28:48 -0400

    first implementation of the without function in without.js

---
+++ without.js
@@ -0,0 +1,40 @@
+// checks if array1 is equal to array 2 returns true or false
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//using eqArrays console logs whether the array passes or fails
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅ Array Assertion Passed.");
+  } else {
+    console.log("🛑 Array Assertion Failed.");
+  }
+};
+
+//takes in the source array and returns a new array
+//new array is the source array with the requested items removed
+const without = function(source, itemsToRemove) {
+  const result = [];  
+  //result = source - itemsToRemove
+  for (let item of source) {
+    if (!itemsToRemove.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+//testing the without function
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
