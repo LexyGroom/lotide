fdb2756243c1aeb50f9fdddb7a204fe53c2f5184
Alexys Groom <lexygroom@gmail.com>
2023-07-26 21:31:04 -0400

    implemented map and added eqArrays and assertArraysEqual

---
+++ map.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅ Array Assertion Passed.");
+  } else {
+    console.log("🛑 Array Assertion Failed.");
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
