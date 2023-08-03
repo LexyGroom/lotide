a3d5c509e960240558cb06a9ec7d76818a0c6d5c
Alexys Groom <lexygroom@gmail.com>
2023-08-03 04:18:31 -0400

    refactored head.js

--- head.js
+++ head.js
@@ -1,6 +1,3 @@
-//compares the 2 values and prints a pass or fail message
-const assertEqual = require('./assertEqual');
-
 //returns the first item in the array, if empty returns undefined
 const head = function(inputArray) {
   if (inputArray.length > 0) {
