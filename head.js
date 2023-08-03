dcf8fd84684c5bd82406261ceb8c080833a2d971
Alexys Groom <lexygroom@gmail.com>
2023-08-03 04:04:15 -0400

    modularized and refactored assertEqual.js and head.js

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
 //compares the 2 values and prints a pass or fail message
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 //returns the first item in the array, if empty returns undefined
 const head = function(inputArray) {
@@ -16,8 +10,4 @@ const head = function(inputArray) {
   }
 };
 
-//testing
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Single"]), "Single");
-assertEqual(head([]), undefined);
+module.exports = head;
