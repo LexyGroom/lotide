cd1a5b1c5b7300e6a37465f5320846d650a2dab9
Alexys Groom <lexygroom@gmail.com>
2023-08-06 13:50:17 -0400

    Add array length check and test case

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,8 @@
 //checks if array1 is equal to array 2 returns true or false
 const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -7,4 +7,5 @@ const eqArrays = require('../eqArrays')
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should pass
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should pass
